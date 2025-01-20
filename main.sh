2d2a6423fb49e69e7175f95302b13d7e3346d6c3
Taiki Endo <te316e89@gmail.com>
2025-01-21 00:32:03 +0900

    Remove links from the title

--- main.sh
+++ main.sh
@@ -166,7 +166,7 @@ if GITHUB_TOKEN="${token}" gh release view "${tag}" &>/dev/null; then
 fi
 
 # https://cli.github.com/manual/gh_release_create
-GITHUB_TOKEN="${token}" retry gh release create "${release_options[@]}" --title "${title}" --notes "${notes}"
+GITHUB_TOKEN="${token}" retry gh release create "${release_options[@]}" --title-no-link "${title}" --notes "${notes}"
 
 # Set (computed) prefix and version outputs for future step use.
 computed_prefix=${tag%"${version}"}
